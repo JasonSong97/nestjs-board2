d749f132ab6e18cd9d72ec1a6f2e26708bdfdae1
Jason_song97 <jaegeunsong97@gmail.com>
2023-10-15 01:49:52 +0900

    ch3 : API 문서화 Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Simple Board')
+    .setDescription('The Simple Board API description')
+    .setVersion('1.0')
+    .addTag('Board') // 컨트롤러 매핑
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // http://localhost:3000/api
+
   await app.listen(3000);
 }
 bootstrap();
