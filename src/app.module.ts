ec59287729b94be49aeceea35320a156d5d766b6
Jason_song97 <jaegeunsong97@gmail.com>
2023-10-15 16:21:46 +0900

    ch5 : PostgresSQL 설치 및 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,22 @@ import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { LoggingMiddleware } from './middleware/logging.middleware';
 import ConfigModule from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     // 상단: 1번만 호출, 특정한 경우
     ConfigModule(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'fastcampus',
+      password: 'fastcampus',
+      database: 'postgres',
+      entities: [__dirname + '/**/*.entity.{.ts,.js}'], // build 시 js로 변경
+      synchronize: false, // entity 변경시 DB에 반영?
+    }),
 
     // 하딘: 도메인 성격(계속추가되는 경우)
     BoardModule
